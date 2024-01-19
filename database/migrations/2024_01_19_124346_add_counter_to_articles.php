930016e81811d73b2c4f05dd4697a031808002c0
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-01-19 14:04:20 +0100

    fix views on article having a default value for creation

--- database/migrations/2024_01_19_124346_add_counter_to_articles.php
+++ database/migrations/2024_01_19_124346_add_counter_to_articles.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->integer('views');
+            $table->integer('views')->default(0);
         });
     }
 
